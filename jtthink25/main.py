e591c2404704a730ebac8ed2bff8ac0b5661d7a0
Carpcai <563998753@qq.com>
2018-02-25 20:55:22 +0800

    add jtthink lesson 25

--- jtthink25/main.py
+++ jtthink25/main.py
@@ -1,6 +1,7 @@
 # 用户口味计算、修正的余弦相似性、利用sklearn实现相关功能
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 users = np.array([3, 2, 1, 2, 3], [6, 4, 3, 5, 6], [2,3,4,5,6])
 
-print(cosine_similarity(users));
+print(cosine_similarity(users));        # cosine_similarity ,计算
