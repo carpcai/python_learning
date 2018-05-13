b57e223e04c19fc86ca4796ab055ac3ebdb661db
carpcai <563998753@qq.com>
2018-05-13 23:27:40 +0800

    Fix

--- numpy02/main.py
+++ numpy02/main.py
@@ -1,3 +1,18 @@
 import numpy as np
+import sys
 
-arr = np.array([1000,1500,800,2000,900,1200,20000,1400])
+arr=np.array([[100, 200, 150, 90, 300, 400], [200, 120, 230, 80, 100, 9000]])
+
+print(arr)
+print(arr.ndim)
+print(arr.shape)
+
+arr=arr.reshape(12)
+print(np.median(np.sort(arr)))  # 排序查中位数
+
+
+
+# arr2 = np.array([1, 2, 3, 4, 5, 6])
+# arr2.shape(3, 2)
+# print(123)
+# print(arr2)
---
+++ tensorflow01/main.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+# 引入TensorFlow库
+import tensorflow01 as tf
+
+# 创建一个常量
+hw = tf.constant("Hello World ! I love TensorFlow")
+
+# 启动一个TensorFlow 的 Session(回话)
+sess = tf.Session()
+
+# 运行Graph(计算图)
+print(sess.run(hw))
+
+sess.close()
